ec0140873fe2058b52301f7de7ce5b047b7333f2
devikakarpe <devikankarpe@gmail.com>
2023-03-14 14:30:52 +0530

    added logic for checking employee presenty

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Program");
+        Random random = new Random();
+        int employeeCheck = random.nextInt(3);
+        if(employeeCheck >=2) {
+            System.out.println("Employee is Present");
+        }
+        else
+            System.out.println("Employee is Absent");
     }
 }
